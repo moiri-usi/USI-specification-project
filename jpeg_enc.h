b403fb93a89a89721a3332b1327a4d8c4e571a38
ouchboy <ouchboy@yahoo.com>
2013-12-04 15:18:39 +0100

    functioning code.
    make sure time is increased 2000000 works

--- jpeg_enc.h
+++ jpeg_enc.h
@@ -8,8 +8,11 @@
 #include	"zz_enc.h"
 #include	"rl_enc.h"
 #include	"P2FF.h"
-#include 	"P2FF.h"
+#include 	"FF2P.h"
 
+#define      block1   1
+#define     block2    1
+#define     block3    1
 
 SC_MODULE(jpeg_enc) {
 
@@ -32,35 +35,39 @@ SC_MODULE(jpeg_enc) {
   zz_enc zz_enc_1;
   rl_enc rl_enc_1;
 
-  //between dct and quantize
+#if block1
   P2FF<float> p2ff_dct_quantize;
-  FF2P<float> ff2p_dct_quantize;
- // fifo_stat<float> 	fifo_dct_quantize;
-  fifo_stat<float> 	dct_quantize_out;
-  sc_signal<float>  float_dct_quantize;
-  sc_signal<bool> bool_dct_quantize_ask;
-  sc_signal<bool> bool_dct_quantize_ready;
+    FF2P<float> ff2p_dct_quantize;
+   // fifo_stat<float> 	fifo_dct_quantize;
+    fifo_stat<float> 	dct_quantize_out;
+    sc_signal<float>  float_dct_quantize;
+    sc_signal<bool> bool_dct_quantize_ask;
+    sc_signal<bool> bool_dct_quantize_ready;
+
+  #endif
 
 
-  //between quantize and zz
+#if block2
   P2FF<int> p2ff_quantize_zz;
-  FF2P<int> ff2p_quantize_zz;
-  //fifo_stat<int> 	fifo_quantize_zz;
-    fifo_stat<int> 	quantize_zz_out;
-    sc_signal<int>  int_quantize_zz;
-    sc_signal<bool> bool_quantize_zz_ask;
-    sc_signal<bool> bool_quantize_zz_ready;
-/*
-  //between zz and rl_enc;
-  P2FF<int> p2ff_zz_rl_enc;
-  FF2P<int> ff2p_zz_rl_enc;
-  fifo_stat<int> 	zz_rl_enc_out;
-  sc_signal<int>  int_zz_rl_enc;
-     sc_signal<bool> bool_zz_rl_enc_ask;
-     sc_signal<bool> bool_zz_rl_enc_ready;
-     */
+    FF2P<int> ff2p_quantize_zz;
+      fifo_stat<int> 	quantize_zz_out;
+      sc_signal<int>  int_quantize_zz;
+      sc_signal<bool> bool_quantize_zz_ask;
+      sc_signal<bool> bool_quantize_zz_ready;
+#endif
 
 
+#if block3
+    //redo
+
+    FF2P<int> ff2p_zz_rl_enc;
+    P2FF<int> p2ff_zz_rl_enc;
+    sc_signal<int> int_zz_rl_enc;
+    sc_signal<bool> bool_zz_rl_enc_ask;
+    sc_signal<bool> bool_zz_rl_enc_ready;
+    fifo_stat<int> zz_rl_enc_out;
+
+#endif
   jpeg_enc(sc_module_name _name, int* _quantization, int _maxwidth):
 		sc_module(_name),
 		r2b_out("r2b_out",1),
@@ -71,13 +78,23 @@ SC_MODULE(jpeg_enc) {
 		dct_1("dct_1"),
 		quant_1("quant_1",_quantization),
 		zz_enc_1("zz_enc_1"),
-		rl_enc_1("rl_enc_1"),
-		p2ff_dct_quantize("p2ff_dct_quantize_"),
-		ff2p_dct_quantize("ff2p_dct_quantize_"),
-		p2ff_quantize_zz("p2ff_quantize_zz"),
-		ff2p_quantize_zz("ff2p_quantize_zz")/*,
-		p2ff_zz_rl_enc("p2ff_zz_rl_enc"),
-		ff2p_zz_rl_enc("ff2p_zz_rl_enc")*/
+		rl_enc_1("rl_enc_1")
+#if block1
+    ,p2ff_dct_quantize("p2ff_dct_quantize_"),
+	ff2p_dct_quantize("ff2p_dct_quantize_"),
+	dct_quantize_out("dct_quantize_out",1)
+#endif
+#if block2
+		,p2ff_quantize_zz("p2ff_quantize_zz"),
+		ff2p_quantize_zz("ff2p_quantize_zz"),
+		quantize_zz_out("quantize_zz_out",1)
+ #endif
+
+#if block3
+ 	,ff2p_zz_rl_enc("ff2p_zz_rl_enc"),
+    p2ff_zz_rl_enc("p2ff_zz_rl_enc"),
+    zz_rl_enc_out("zz_rl_enc_out",1)
+#endif
 		{
 
 	r2b_1.input(input);
@@ -87,52 +104,72 @@ SC_MODULE(jpeg_enc) {
 	dct_1.input(r2b_out);
 	dct_1.output(dct_out);
 
-	ff2p_dct_quantize.clk(clk);
-	ff2p_dct_quantize.input(dct_out);
-	ff2p_dct_quantize.output(float_dct_quantize);
-	ff2p_dct_quantize.ask(bool_dct_quantize_ask);
-	ff2p_dct_quantize.ready(bool_dct_quantize_ready);
+#if block1
+	    ff2p_dct_quantize.clk(clk);
+		ff2p_dct_quantize.input(dct_out);
+		ff2p_dct_quantize.output(float_dct_quantize);
+		ff2p_dct_quantize.ask(bool_dct_quantize_ask);
+		ff2p_dct_quantize.ready(bool_dct_quantize_ready);
 
-	p2ff_dct_quantize.clk(clk);
-	p2ff_dct_quantize.input(float_dct_quantize);
-	p2ff_dct_quantize.ask(bool_dct_quantize_ask);
-    p2ff_dct_quantize.ready(bool_dct_quantize_ready);
-	p2ff_dct_quantize.output(dct_quantize_out);
+		p2ff_dct_quantize.clk(clk);
+		p2ff_dct_quantize.input(float_dct_quantize);
+		p2ff_dct_quantize.ask(bool_dct_quantize_ask);
+	    p2ff_dct_quantize.ready(bool_dct_quantize_ready);
+		p2ff_dct_quantize.output(dct_quantize_out);
+		quant_1.input(dct_quantize_out);
+	#endif
 
 
-	quant_1.input(dct_quantize_out);
+#if !block1
+	quant_1.input(dct_out);
+	#endif
+
 	quant_1.output(quant_out);
-
+#if block2
 	ff2p_quantize_zz.input(quant_out);
 	ff2p_quantize_zz.clk(clk);
 	ff2p_quantize_zz.output(int_quantize_zz);
 	ff2p_quantize_zz.ask(bool_quantize_zz_ask);
-	ff2p_quantize_zz.ready(bool_quantize_zz_ready);
+    ff2p_quantize_zz.ready(bool_quantize_zz_ready);
 
-	p2ff_quantize_zz.input(int_quantize_zz);
-	p2ff_quantize_zz.clk(clk);
-	p2ff_quantize_zz.output(quantize_zz_out);
-	p2ff_quantize_zz.ask(bool_quantize_zz_ask);
-	p2ff_quantize_zz.ready(bool_quantize_zz_ready);
 
-	zz_enc_1.input(quantize_zz_out);
+		    p2ff_quantize_zz.input(int_quantize_zz);
+			p2ff_quantize_zz.clk(clk);
+			p2ff_quantize_zz.output(quantize_zz_out);
+			p2ff_quantize_zz.ask(bool_quantize_zz_ask);
+			p2ff_quantize_zz.ready(bool_quantize_zz_ready);
+
+			zz_enc_1.input(quantize_zz_out);
+#endif
+
+#if !block2
+	zz_enc_1.input(quant_out);
+#endif
+
 	zz_enc_1.output(zz_enc_out);
-/*
+
+#if block3
+
 	ff2p_zz_rl_enc.input(zz_enc_out);
 	ff2p_zz_rl_enc.clk(clk);
-	ff2p_zz_rl_enc.output(int_zz_rl_enc);
 	ff2p_zz_rl_enc.ask(bool_zz_rl_enc_ask);
 	ff2p_zz_rl_enc.ready(bool_zz_rl_enc_ready);
-	*/
-/*
+
+	ff2p_zz_rl_enc.output(int_zz_rl_enc);
+
 	p2ff_zz_rl_enc.input(int_zz_rl_enc);
 	p2ff_zz_rl_enc.clk(clk);
-	p2ff_zz_rl_enc.output(zz_rl_enc_out);
 	p2ff_zz_rl_enc.ask(bool_zz_rl_enc_ask);
 	p2ff_zz_rl_enc.ready(bool_zz_rl_enc_ready);
-*/
 
+	p2ff_zz_rl_enc.output(zz_rl_enc_out);
+
+	rl_enc_1.input(zz_rl_enc_out);
+
+#endif
+#if !block3
 	rl_enc_1.input(zz_enc_out);
+#endif
 	rl_enc_1.output(output);
 
 		}
