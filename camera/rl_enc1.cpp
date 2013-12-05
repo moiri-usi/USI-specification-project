a5738757788111e0807f2bcd0a18353481d3dbd3
Simon Maurer <moirelein@gmail.com>
2013-12-05 17:42:54 +0100

    completed RTL version of rl_encoder

--- camera/rl_enc1.cpp
+++ camera/rl_enc1.cpp
@@ -1,58 +1,148 @@
 #include "rl_enc1.h"
 
-void rl_enc1::process() {
+void rl_enc1::process(){
 
-    int     count;
-    int     k;
-    int     value;
+    sc_int<32> value2;
+    sc_int<32> value3;
 
-    while (1) {
-        // read and write DC value
-        ask_i.write(true);
+    if (reset.read() == true ){
+        state.write(RESET);
         ready_o.write(false);
-        wait();
-        while (!ready_i.read()) wait();
-        value = input.read();
         ask_i.write(false);
-        wait();
-        while (!ask_o.read()) wait();
-        output.write(value);
-        ready_o.write(true);
-        wait();
+    } else {
+        switch (state.read()){
 
-        count = 0;
 
-        for( k = 1 ; k < 64 ; k++ ) {
-            ask_i.write(true);
-            ready_o.write(false);
-            wait();
-            while (!ready_i.read()) wait();
-            value = input.read();
-            ask_i.write(false);
-            wait();
-            if ( value == 0 ) {
-                count++;
-            }
-            else {
-                while (!ask_o.read()) wait();
-                output.write(count);
-                ready_o.write(true);
-                wait();
-                count = 0;
+            case RESET: 
+                ask_i.write(true);      //indicate that we want to read in next cycle
+                ready_o.write(false);       //indicate that we do not write data in this cycle
+                state.write(READZIGZAG);
+                count.write(0);
+                increment.write(0);
+                break;
+
+            case READZIGZAG:
+
                 ready_o.write(false);
-                wait();
-                while (!ask_o.read()) wait();
-                output.write(value);
-                ready_o.write(true);
-                wait();
-            }
+                increment.write(1);
+
+                if (ready_i.read()==true){
+                    value.write(input.read());
+                    ask_i.write(false);
+                    state.write(WRITEDC);
+                }
+                else {
+                    ask_i.write(true);//fine!!!
+                    state.write(READZIGZAG);
+                }
+                break;
+
+            case WRITEDC:
+                //cout<< "writedc_state"<< endl;
+                //cout<< "writedc_state"<< endl;
+                if(ask_o.read()==true){
+                    output.write(value.read());
+
+                    ask_i.write(false); // check it out!!
+
+                    ready_o.write(true);//count to zeros
+                    count.write(0);
+                    state.write(WAITFORZEROS);
+                }
+                else{
+                    ready_o.write(false);//
+                    ask_i.write(false);///
+                    state.write(WRITEDC);
+                }
+                break;
+
+                //  case WAITFORZEROS:
+
+            case WAITFORZEROS: //wait for succesfull write AC/DC value
+
+                ready_o.write(false);
+                if (increment.read() < ((sc_int<32>)(64))){
+                    ask_i.write(true);
+                    value3 = increment.read();
+                    value3++;
+                    increment.write(value3);
+
+                    state.write(READZEROS);
+                }
+                else{
+
+                    state.write(TERMINATION);
+                }
+                break;
+
+
+            case READZEROS:
+
+                if(ready_i.read()== true){ // ojo after
+
+                    value.write(input.read());
+                    ask_i.write(false);
+                    state.write(COUNTING);
+                }
+                else{
+                    state.write(READZEROS);
+                }
+
+                break;
+
+            case COUNTING:
+
+                if (value.read()== (sc_int<32>)(0)){
+                    value2 = count.read();
+                    value2++;
+                    count.write(value2);
+                    state.write(WAITFORZEROS);
+                }
+                else{
+                    state.write(WRITECOUNT);
+                }
+
+
+
+                break;
+
+            case WRITECOUNT:
+
+                ready_o.write(false);
+                ask_i.write(false);
+
+                if (ask_o.read() == true){
+                    output.write(count.read());
+                    ready_o.write(true);
+                    state.write(WAITTOWRITE);
+                }
+                else{
+                    state.write(WRITECOUNT);
+                }
+
+                break;
+
+            case WAITTOWRITE:
+
+                ready_o.write(false);
+                ask_i.write(false);
+                state.write(WRITEDC);
+                break;
+
+
+            case TERMINATION:
+
+                if(ask_o.read() == true){
+                    output.write((sc_int<32>)(63));
+                    ready_o.write(true);
+                    ask_i.write(true);
+                    state.write(READZIGZAG);
+                }
+                else{
+                    state.write(TERMINATION);
+                }
+                break;
+
         }
-        ready_o.write(false);
-        wait();
-        while (!ask_o.read()) wait();
-        output.write(static_cast<sc_int<32> >(63));
-        ready_o.write(true);
-        wait();
     }
 }
-
