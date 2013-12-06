2c6c316f0f01abb4099feddea09df0a021aa1a29
Simon Maurer <moirelein@gmail.com>
2013-12-06 01:21:09 +0100

    use a new version off zz_enc with merged protocol

--- camera/zz_enc1.cpp
+++ camera/zz_enc1.cpp
@@ -2,52 +2,47 @@
 
 void zz_enc1::process() {
 
-	int		i, j;
-	int		temp_block[64];
-	int		block[64];
+    int     i, j;
+    int     temp_block[64];
+    int     block[64];
 
+    int zig_zag[64] = {
+        0, 1, 8, 16, 9, 2, 3, 10,
+        17, 24, 32, 25, 18, 11, 4, 5,
+        12, 19, 26, 33, 40, 48, 41, 34,
+        27, 20, 13, 6, 7, 14, 21, 28,
+        35, 42, 49, 56, 57, 50, 43, 36,
+        29, 22, 15, 23, 30, 37, 44, 51,
+        58, 59, 52, 45, 38, 31, 39, 46,
+        53, 60, 61, 54, 47, 55, 62, 63};
 
-	int zig_zag[64] = {0,1,8,16,9,2,3,10 ,
-			    		17,24,32,25,18,11,4,5 ,
-			    		12,19, 26, 33, 40, 48, 41, 34,
-			    		27, 20, 13, 6, 7, 14, 21, 28,
-			    		35, 42, 49, 56, 57, 50, 43, 36,
-			    		29, 22, 15, 23, 30, 37, 44, 51,
-			    		58, 59, 52, 45, 38, 31, 39, 46,
-			    		53, 60, 61, 54, 47, 55, 62, 63};
+    while(1) {
+        //read in the blocks for 8 lines
+        ask_i.write(true);
+        ready_o.write(false);
+        wait();
 
-	while(1) {
-		//read in the blocks for 8 lines
-		ask_i.write(true);
-		ready_o.write(false);wait();
+        for ( i = 0 ; i < 64 ; i ++) {
+            ask_i.write(true);
+            wait();
+            while (!ready_i.read()) wait();
+            ready_o.write(false);
+            temp_block[i]= input.read();
+            ask_i.write(false);
+            wait();
+        }
 
-
-		for ( i = 0 ; i < 64 ; i ++) {
-			ask_i.write(true);wait();
-
-			while (!ready_i.read()) wait();
-
-			ready_o.write(false);
-	    	temp_block[i]= input.read();
-			ask_i.write(false);
-			wait();
-
-		}
-
-	    for (j=0; j < 64 ; j++){
-	    	ready_o.write(false);wait();
-
-	    	while (!ask_o.read())wait();
-
-	    	block[j]= temp_block[zig_zag[j]];
-	    	output.write (block[j]);
-	    	ready_o.write(true);
-	    	wait();
-
-	    }
-
-	    ready_o.write(false);wait();
-
-	}
+        for (j=0; j < 64 ; j++){
+            ready_o.write(false);
+            wait();
+            while (!ask_o.read()) wait();
+            block[j] = temp_block[zig_zag[j]];
+            output.write (block[j]);
+            ready_o.write(true);
+            wait();
+        }
+        ready_o.write(false);
+        wait();
+    }
 }
 
--- camera/zz_enc1.h
+++ camera/zz_enc1.h
@@ -1,30 +1,26 @@
 //*  rl_enc.h */
+#ifndef _ZZ_ENC1
+#define _ZZ_ENC1
 #include <systemc.h>
 #include "add2systemc.h"
 
 SC_MODULE(zz_enc1) {
 
+    // ports
+    sc_out< sc_int<32> >    output;
+    sc_in<bool>             ask_o;
+    sc_out<bool>            ready_o;
+    sc_in< sc_int<9> >      input;
+    sc_in<bool>             ready_i;
+    sc_out<bool>            ask_i;
+    sc_in<bool>             clk;
 
-	// ports
+    void process();
 
-	sc_out< sc_int<32> >	output;
-	sc_in<bool>				ask_o;
-	sc_out<bool>			ready_o;
-	sc_in< sc_int<32> >		input;
-	sc_in<bool>				ready_i;
-	sc_out<bool>			ask_i;
-	sc_in<bool>				clk;
-
-	void process();
-
-
-  SC_CTOR(zz_enc1){
-			SC_THREAD(process);
-			sensitive << clk.pos();
-  }
-
+    SC_CTOR(zz_enc1){
+        SC_THREAD(process);
+        sensitive << clk.pos();
+    }
 };
 
-
-
-
+#endif
